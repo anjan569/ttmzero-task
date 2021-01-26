13490824a2b4804c0fb9bca09efcc63023560370
Anjan <anjan569@gmail.com>
2021-01-26 15:17:58 +0530

    reorder

--- ttmzero-app/src/app/custom-filters.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomFiltersPipe } from './custom-filters.pipe';
-
-describe('CustomFiltersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CustomFiltersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
